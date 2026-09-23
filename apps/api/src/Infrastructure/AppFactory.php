61176c3cf5f51357962930953656ddfd778e0183
1S Duarte <duartecdsrj@gmail.com>
2026-09-23 15:27:49 -0300

    feat(catalog): expor assuntos por edital

--- apps/api/src/Infrastructure/AppFactory.php
+++ apps/api/src/Infrastructure/AppFactory.php
@@ -37,6 +37,7 @@ use App\Interface\Http\Catalog\CatalogRequestFactory;
 use App\Interface\Http\Catalog\Controller\CatalogController;
 use App\Interface\Http\Catalog\CatalogRouteRegistrar;
 use App\Interface\Http\Catalog\TagRouteRegistrar;
+use App\Interface\Http\Catalog\SubjectRouteRegistrar;
 use App\Interface\Http\QuestionBank\Controller\QuestionImportController;
 use App\Interface\Http\QuestionBank\QuestionImportRequestFactory;
 use App\Interface\Http\Study\StudyRouteRegistrar;
@@ -60,6 +61,7 @@ final class AppFactory
         $catalog = self::catalogController($responses, self::authService());
         (new CatalogRouteRegistrar($responses, self::authService()))->register($app);
         (new TagRouteRegistrar($responses, self::authService()))->register($app);
+        (new SubjectRouteRegistrar($responses, self::authService()))->register($app);
         $importRequests = new QuestionImportRequestFactory();
         (new StudyRouteRegistrar($responses, self::authService()))->register($app);
         $imports = self::questionImportController($responses, self::authService());
